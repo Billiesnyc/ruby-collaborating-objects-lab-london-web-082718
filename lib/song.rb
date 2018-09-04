1b38b37444dd830a13edf48b22d4490f2d80ff25
Billie Simmons <billiesnyc@gmail.com>
2018-09-04 14:51:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,15 @@ class Song
     @name = name
   end
   
-  def new_by_filename(file)
-    
+  def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
   end
-  
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 end
