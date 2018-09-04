53dd6c7cc7e8ed76e8ea12e07dff3360dbae0113
Billie Simmons <billiesnyc@gmail.com>
2018-09-04 14:50:38 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,4 +16,18 @@ class Artist
   def self.save
     @@all << self
   end
+  
+    def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+
+  def self.create(name)
+    self.new(name).tap {|artist| artist.save}
+  end
+
 end 
